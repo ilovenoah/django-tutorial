7f85a2a0d91a8ef6fa37ffbfec6d63b4459cc296
taiphyyy <ilovenoah0105@gmail.com>
2024-04-25 18:37:42 +0900

    changed permission

--- requirements/django/entrypoint.sh
+++ requirements/django/entrypoint.sh
@@ -6,7 +6,9 @@ sed -i "s/POSTGRES_USER/$POSTGRES_USER/g" "./mysite/.my_pgpass"
 sed -i "s/POSTGRES_PASSWORD/$POSTGRES_PASSWORD/g" "./mysite/.my_pgpass"
 sed -i "s/POSTGRES_DB/$POSTGRES_DB/g" "./mysite/.pg_service.conf"
 sed -i "s/POSTGRES_USER/$POSTGRES_USER/g" "./mysite/.pg_service.conf"
-cp -f ./mysite/.pg_service.conf ~/
+chmod 0600 /code/mysite/.my_pgpass
+chmod 0600 /code/mysite/.pg_service.conf
+cp -f /code/mysite/.pg_service.conf ~/.pg_service.conf
 
 while true; do
   sleep 100
