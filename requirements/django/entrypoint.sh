36b50a85f50932a86cc27ff3e889120aa037ec7e
taiphyyy <ilovenoah0105@gmail.com>
2024-04-25 17:01:25 +0900

    Added entrypoint.sh to django container Get information needed to connect to db from environment variables

---
+++ requirements/django/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x
+
+sed -i "s/POSTGRES_DB/$POSTGRES_DB/g" "./mysite/.my_pgpass"
+sed -i "s/POSTGRES_USER/$POSTGRES_USER/g" "./mysite/.my_pgpass"
+sed -i "s/POSTGRES_PASSWORD/$POSTGRES_PASSWORD/g" "./mysite/.my_pgpass"
+sed -i "s/POSTGRES_DB/$POSTGRES_DB/g" "./mysite/.pg_service.conf"
+sed -i "s/POSTGRES_USER/$POSTGRES_USER/g" "./mysite/.pg_service.conf"
+cp -f ./mysite/.pg_service.conf ~/
+
+while true; do
+  sleep 100
+done
